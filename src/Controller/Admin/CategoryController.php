6d16240aa2e559406afcc8d204aff5b6640edba4
mobize <olivier.charpentier@icloud.com>
2018-03-02 17:30:53 +0100

    Blog symfony 02/03
    Premier envoi

---
+++ src/Controller/Admin/CategoryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+* @Route("/category")
+*/
+class CategoryController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        return $this->render('admin/category/index.html.twig', 
+        [
+            
+        ]);
+    }
+}
