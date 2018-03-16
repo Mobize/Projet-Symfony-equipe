119f92cd9c0543c7e4e105bfb6c6cafb74775f9e
mobize <olivier.charpentier@icloud.com>
2018-03-16 19:12:04 +0100

    remise en fonction

--- src/Controller/EquipeController.php
+++ src/Controller/EquipeController.php
@@ -10,7 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  /**
   * @Route("/equipes")
   */
-class EquipesController extends Controller
+class EquipeController extends Controller
 {
    /**
      * @Route("/")
