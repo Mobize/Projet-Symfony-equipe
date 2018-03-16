af4a611b5217bdfc2d079056c252fa44f2e49334
Ariwf3 <bassole.aristide@laposte.net>
2018-03-16 16:51:51 +0100

    mise a jour route 'rencontres'

--- src/Controller/RencontresController.php
+++ src/Controller/RencontresController.php
@@ -2,26 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\Club;
+use App\Entity\Rencontre;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RencontresController extends Controller
 {
     /**
-     * @Route("/rencontres", name="rencontres")
+     * @Route("/rencontres/{id}", name="rencontres", defaults={"id": null})
      */
-    public function index()
+    public function index(Request $request,$id)
     {
-    /*
-     * partie de recuperation des données en suspens pour tests
-     $repository = $this->getDoctrine()->getRepository(Rencontres::class);
-        //on recup ttes les rencontres
-        $rencontres = $repository->findAll();  
-     * 
-    */    
-        return $this->render('rencontres/index.html.twig', [
-            'controller_name' => 'rencontres'
-        ]);
-    }
+     
+     $repository = $this->getDoctrine()->getRepository(Rencontre::class);
+        // recup des rencontres
+        $rencontres = $repository->findAll();
+        
+        $Clubrepository = $this->getDoctrine()->getRepository(Club::class);
+        //recup des clubs
+        $clubs = $Clubrepository->findAll();
+        
+        //entity manager gere les objets et les lignes en bdd
+        $em= $this->getDoctrine()->getManager();
+        
+        if(is_null($id) ) {
+        $club = new Club();
+        } else {
+         $club = $em->getRepository(Club::class)->find($id);
+         
+         if (!is_null($club->getLogo()) ) {
+                $originalImage = $club->getLogo();
+                $imagePath = $this->getParameter('upload_dir') . '/' . $originalImage;
+        } 
+         
+       
+        } // fin else
+     return $this->render('rencontres/index.html.twig',
+         [
+            'rencontres' => $rencontres,
+            'clubs' => $clubs
+         ]);
     
-}
+        }//fin fonction index
+    
+} // fin class controller 
